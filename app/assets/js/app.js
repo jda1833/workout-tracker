9cdde72575d96d36a7e1ef5eea6238c40d1744e5
john <john.d.ansted@gmail.com>
2026-04-03 13:01:04 -0400

    patch

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,10 +1,20 @@
 (function () {
     function init() {
-        window.WorkoutApp.initNav();
-        window.WorkoutApp.initTracker();
-        window.WorkoutApp.initCheckIn();
-        window.WorkoutApp.initUpload();
-        window.WorkoutApp.loadPrograms();
+        if (typeof window.WorkoutApp.initNav === "function") {
+            window.WorkoutApp.initNav();
+        }
+        if (typeof window.WorkoutApp.initTracker === "function") {
+            window.WorkoutApp.initTracker();
+        }
+        if (typeof window.WorkoutApp.initCheckIn === "function") {
+            window.WorkoutApp.initCheckIn();
+        }
+        if (typeof window.WorkoutApp.initUpload === "function") {
+            window.WorkoutApp.initUpload();
+        }
+        if (typeof window.WorkoutApp.loadPrograms === "function") {
+            window.WorkoutApp.loadPrograms();
+        }
     }
 
     window.addEventListener("DOMContentLoaded", init);
